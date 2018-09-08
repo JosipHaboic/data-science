6a3021047dd9effd34c74f6aea183d77b1f296fd
jhaboic <josiphaboic@gmail.com>
2018-09-08 10:25:45 +0200

    (update) use named import

--- __tests__/fn.test.ts
+++ __tests__/fn.test.ts
@@ -1,26 +1,26 @@
-import { max, min } from '../src/fn';
+import * as fn from '../src/fn';
 
 
 test('returns undefined when data is empty list', () => {
-  expect(max([])).toBe(undefined);
+  expect(fn.max([])).toBe(undefined);
 })
 
 test('returns 3 from list of [1, 2, 3]', () => {
-  expect(max([1, 2, 3])).toBe(3);
+  expect(fn.max([1, 2, 3])).toBe(3);
 })
 
 test('returns 3 from list of [3, 2, 1]', () => {
-  expect(max([3, 2, 1])).toBe(3);
+  expect(fn.max([3, 2, 1])).toBe(3);
 })
 
 test('function max: returns undefined when data is empty list', () => {
-  expect(min([])).toBe(undefined);
+  expect(fn.min([])).toBe(undefined);
 })
 
 test('function min: returns 1 from list of [1, 2, 3]', () => {
-  expect(min([1, 2, 3])).toBe(1);
+  expect(fn.min([1, 2, 3])).toBe(1);
 })
 
 test('function min: returns 1 from list of [3, 2, 1]', () => {
-  expect(min([3, 2, 1])).toBe(1);
+  expect(fn.min([3, 2, 1])).toBe(1);
 })
