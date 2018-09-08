cb1cadc4c015728831824b15f7b0909a064761d8
jhaboic <josiphaboic@gmail.com>
2018-09-08 22:34:20 +0200

    (update) tests

--- __tests__/dispersion.test.ts
+++ __tests__/dispersion.test.ts
@@ -1,8 +1,12 @@
 import * as dispersion from '../src/statistics/dispersion';
 
 
-test('function range: returns NaN from list of []', () => {
-  expect(dispersion.range([])).toThrowError('Data is empty list');
+test('function range: throws an TypeError when not passed any data', () => {
+  expect(dispersion.range).toThrowError(TypeError);
+})
+
+test('function range: return -Infinity when passed empty list', () => {
+  expect(dispersion.range([])).toBe(-Infinity);
 })
 
 test('function range: returns 2 from list of [1, 2, 3]', () => {
--- __tests__/summary.test.ts
+++ __tests__/summary.test.ts
@@ -1,62 +1,62 @@
 import * as fn from '../src/statistics/summary';
 
 
-test('function sum: returns NaN from list of []', () => {
-  expect(fn.sum([])).toThrowError('Data is empty list');
+test('function sum: throws TypeError when not passed any data', () => {
+  expect(fn.sum).toThrowError(TypeError);
 })
 
 test('function sum: returns 6 from list of [1, 2, 3]', () => {
   expect(fn.sum([1, 2, 3])).toBe(6);
 })
 
-test('function arithmeticMean: returns NaN from list of []', () => {
-  expect(fn.arithmeticMean([])).toThrowError('Data is empty list');
+test('function arithmeticMean: throws TypeError when not passed any data', () => {
+  expect(fn.arithmeticMean).toThrowError(TypeError);
 })
 
 test('function arithmeticMean: returns 2 from list of [1, 2, 3]', () => {
   expect(fn.arithmeticMean([1, 2, 3])).toBe(2);
 })
 
-test('function median: returns NaN from list of []', () => {
-  expect(fn.median([])).toThrowError('Data is empty list');
+test('function median: throws TypeError when not passed any data', () => {
+  expect(fn.median).toThrowError(TypeError);
 })
 
 test('function median: returns 2 from list of [1, 2, 3]', () => {
   expect(fn.median([1, 2, 3])).toBe(2);
 })
 
-test('function median: returns 2 from list of [1, 2, 3, 4]', () => {
+test('function median: returns 2.5 from list of [1, 2, 3, 4]', () => {
   expect(fn.median([1, 2, 3, 4])).toBe(2.5);
 })
 
-test('function mode: returns NaN from list of []', () => {
-  expect(fn.mode([])).toThrowError('Data is empty list');
+test('function mode: throws TypeError when not passed any data', () => {
+  expect(fn.mode).toThrowError(TypeError);
 })
 
 test('function mode: returns {"1": 1, "2": 2} from list of [1, 2, 2]', () => {
   expect(fn.mode([1, 2, 2])).toEqual({"1": 1, "2": 2});
 })
 
-test('function midrange: returns NaN from list of []', () => {
-  expect(fn.midrange([])).toThrowError('Data is empty list');
+test('function midrange: throws TypeError when not passed any data', () => {
+  expect(fn.midrange).toThrowError(TypeError);
 })
 
-test('function midrange: returns NaN from list of [1, 2, 3]', () => {
+test('function midrange: returns 2 from list of [1, 2, 3]', () => {
   expect(fn.midrange([1, 2, 3])).toBe(2);
 })
 
-test('function geometricMean: returns NaN from list of []', () => {
-  expect(fn.geometricMean([])).toThrowError('Data is empty list');
+test('function geometricMean: throws TypeError when not passed any data', () => {
+  expect(fn.geometricMean).toThrowError(TypeError);
 })
 
 test('function geometricMean: returns 1.8171205928321 from list of [1, 2, 3]', () => {
   expect(fn.geometricMean([1, 2, 3])).toBeCloseTo(1.8171205928321);
 })
 
-test('function harmonicMean: returns NaN from list of []', () => {
-  expect(fn.harmonicMean([])).toThrowError('Data is empty list');
+test('function harmonicMean: throws TypeError when not passed any data', () => {
+  expect(fn.harmonicMean).toThrowError(TypeError);
 })
 
-test('function harmonicMean: returns NaN from list of [1, 2, 3]', () => {
+test('function harmonicMean: returns 1.6363636363636 from list of [1, 2, 3]', () => {
   expect(fn.harmonicMean([1, 2, 3])).toBeCloseTo(1.6363636363636);
 })
