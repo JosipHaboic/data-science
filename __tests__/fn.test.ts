21492c62bb913df3eecbbb3e7bfaec1bbf045236
jhaboic <josiphaboic@gmail.com>
2018-09-08 15:44:27 +0200

    (refactor) remove min and max and add mode and midrange

--- __tests__/fn.test.ts
+++ __tests__/fn.test.ts
@@ -1,30 +1,6 @@
 import * as fn from '../src/fn';
 
 
-test('returns NaN when data is empty list', () => {
-  expect(fn.max([])).toBe(NaN);
-})
-
-test('returns 3 from list of [1, 2, 3]', () => {
-  expect(fn.max([1, 2, 3])).toBe(3);
-})
-
-test('returns 3 from list of [3, 2, 1]', () => {
-  expect(fn.max([3, 2, 1])).toBe(3);
-})
-
-test('function max: returns NaN when data is empty list', () => {
-  expect(fn.min([])).toBe(NaN);
-})
-
-test('function min: returns 1 from list of [1, 2, 3]', () => {
-  expect(fn.min([1, 2, 3])).toBe(1);
-})
-
-test('function min: returns 1 from list of [3, 2, 1]', () => {
-  expect(fn.min([3, 2, 1])).toBe(1);
-})
-
 test('function sum: returns NaN from list of []', () => {
   expect(fn.sum([])).toBe(NaN);
 })
@@ -53,10 +29,18 @@ test('function median: returns 2 from list of [1, 2, 3, 4]', () => {
   expect(fn.median([1, 2, 3, 4])).toBe(2.5);
 })
 
-test('function count: returns undefined from list of []', () => {
-  expect(fn.count([])).toBe(undefined);
+test('function mode: returns NaN from list of []', () => {
+  expect(fn.mode([])).toBe(NaN);
 })
 
-test('function count: returns 2 from list of [1, 2, 2]', () => {
-  expect(fn.count([1, 2, 2])).toBe({'1': 2, '2': 2});
+test('function mode: returns {"1": 1, "2": 2} from list of [1, 2, 2]', () => {
+  expect(fn.mode([1, 2, 2])).toEqual({"1": 1, "2": 2});
+})
+
+test('function midrange: returns NaN from list of []', () => {
+  expect(fn.midrange([])).toBe(NaN);
+})
+
+test('function midrange: returns NaN from list of [1, 2, 3]', () => {
+  expect(fn.midrange([1, 2, 3])).toBe(2);
 })
--- src/fn/index.ts
+++ src/fn/index.ts
@@ -1,52 +1,26 @@
 /**
  *
- * @param data - holds numeric data in a list
- * @returns largest number in list
- */
-export function max(data: number[]): number {
-  if (data.length === 0)  { return undefined; }
-
-  return data.reduce((previous: number, next: number) => {
-    return previous >= next ? previous : next;
-  });
-}
-
-/**
- *
- * @param data - holds numeric data in a list
- * @returns smallest number in list
- */
-export function min(data: number[]): number {
-  if (data.length === 0)  { return undefined; }
-
-  return data.reduce((previous: number, next: number) => {
-    return previous <= next ? previous : next;
-  });
-}
-
-/**
- *
- * @param data - holds numeric data in a list
- * @returns sum of data
+ * @param number list
+ * @returns number
  */
 export function sum(data: number[]): number {
-  if (data.length === 0)  { return undefined; }
+  if (data.length === 0)  { return NaN; }
 
-  return data.reduce((previous: number, next: number) => {
-    return previous + next;
+  return data.reduce((previous: number, current: number) => {
+    return previous + current;
   }, 0);
 }
 
 /**
  *
- * @param data - holds numeric data in a list
- * @returns arithmetic mean of data
+ * @param number list
+ * @returns number
  */
 export function mean(data: number[]): number {
-  if (data.length === 0)  { return undefined; }
+  if (data.length === 0)  { return NaN; }
 
-  const sumOfData = data.reduce((previous: number, next: number) => {
-    return previous + next;
+  const sumOfData = data.reduce((previous: number, current: number) => {
+    return previous + current;
   }, 0);
 
   return sumOfData / data.length;
@@ -54,11 +28,11 @@ export function mean(data: number[]): number {
 
 /**
  *
- * @param data - holds numeric ordered data in a list
- * @returns true if data lenght is even false otherwise
+ * @param number list
+ * @returns number
  */
 export function median(data: number[]): number {
-  if (data.length === 0)  { return undefined; }
+  if (data.length === 0)  { return NaN; }
   const length = data.length;
   const isOdd = (length + 1) % 2 === 0;
   if (isOdd) {
@@ -66,4 +40,37 @@ export function median(data: number[]): number {
   } else {
     return mean([data[length / 2], data[(length / 2) - 1]]);
   }
+}
+
+/**
+ *
+ * @param number list
+ * @returns number or NaN
+ */
+export function mode(data: number[]): Object {
+  if (data.length === 0)  { return NaN; }
+  const counter: Object = new Object();
+
+  for (const item of data) {
+    if (counter[item]) {
+      // tslint:disable-next-line
+      counter[item] += 1;
+    } else {
+      // tslint:disable-next-line
+      counter[item] = 1;
+    }
+  }
+
+  return counter;
+}
+
+/**
+ *
+ * @param number list
+ * @returns number
+ */
+export function midrange(data: number[]): number {
+  if (data.length === 0) { return NaN; }
+
+  return mean([Math.min(...data), Math.max(...data)]);
 }
