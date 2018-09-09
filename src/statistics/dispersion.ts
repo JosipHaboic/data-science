81788f480746fb2ce50a747587528d8936e3a2ca
jhaboic <josiphaboic@gmail.com>
2018-09-10 00:27:42 +0200

    (add) standardDeviation and variance functions

--- src/statistics/dispersion.ts
+++ src/statistics/dispersion.ts
@@ -1,4 +1,7 @@
+import { arithmeticMean } from './summary';
+
 /**
+ * Calcaulte range of data
  * @param {array} data - List of numerical data
  * @returns {number} - Range of data
  */
@@ -7,10 +10,49 @@ export function range(data: number[]): number {
 }
 
 /**
- * @param {array} data - List of numerical data
- * @returns {number} - Variance of data
+ * Calculate standard deviation from variance
+ * @param {number} variance - variance of data
+ * @returns {number} - Standard deviation of data
  */
-export function range(data: number[]): number {
-  return Math.max(...data) - Math.min(...data);
+export function standardDeviation(variance: number) {
+  return Math.sqrt(variance);
 }
 
+
+export namespace Population {
+
+  /**
+   * Calculate variance of population
+   * @param {array} data - List of numerical data
+   * @returns {number} - Variance of population data
+   */
+  export function variance(data: number[]): number {
+    const populationMean = arithmeticMean(data);
+
+    return (1 / data.length) * data.reduce(
+      (previous: number, current: number) => {
+        return previous + Math.pow(current - populationMean, 2);
+      }, 0
+    );
+  }
+
+}
+
+export namespace Sample {
+
+  /**
+   * Calculate variance of sample
+   * @param {array} data - List of numerical data
+   * @returns {number} - Variance of sample data
+   */
+  export function variance(data: number[]): number {
+    const populationMean = arithmeticMean(data);
+
+    return (1 / (data.length - 1)) * data.reduce(
+      (previous: number, current: number) => {
+        return previous + Math.pow(current - populationMean, 2);
+      }, 0
+    );
+  }
+
+}
