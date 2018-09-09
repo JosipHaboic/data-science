49f2e38e29e97852f7be27efd55af5d23c7d36a5
jhaboic <josiphaboic@gmail.com>
2018-09-09 18:08:19 +0200

    (add) more tests

--- __tests__/models/labeled.test.ts
+++ __tests__/models/labeled.test.ts
@@ -10,3 +10,21 @@ test('model:LabeledData should set label = "age" and value = 36', () => {
   expect(age.label).toBe('age');
   expect(age.value).toBe(36);
 });
+
+test('model:LabeledData should set label = "is-online" and value = false', () => {
+  const age = new labeled.LabeledData('is-online', false);
+  expect(age.label).toBe('is-online');
+  expect(age.value).toBe(false);
+});
+
+test('model:LabeledData should set label = "is-online" and value = true', () => {
+  const age = new labeled.LabeledData('is-online', true);
+  expect(age.label).toBe('is-online');
+  expect(age.value).toBe(true);
+});
+
+test('model:LabeledData should set label = "name" and value = "Joe"', () => {
+  const age = new labeled.LabeledData('name', 'Joe');
+  expect(age.label).toBe('name');
+  expect(age.value).toBe('Joe');
+});
