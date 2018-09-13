79a20bdbeab391cae74d750f8da8232a90d269ca
jhaboic <josiphaboic@gmail.com>
2018-09-13 20:53:28 +0200

    (add) random:range

---
+++ src/random/range.ts
@@ -0,0 +1,13 @@
+export function range(start: number = 0, end: number = 1, n: number = 10) {
+  const data = [];
+  let item = 0;
+  while (data.length < n) {
+    // tslint:disable-next-line
+    item = Math.random() * end;
+    if (item > start) {
+      data.push(item);
+    }
+  }
+
+  return data;
+}
