bbe2762dce746ab08d21cb029bf98e21b0cfa095
jhaboic <josiphaboic@gmail.com>
2018-09-15 00:54:23 +0200

    (update) add docstring

--- src/random/range.ts
+++ src/random/range.ts
@@ -1,3 +1,9 @@
+/**
+ * Create an range of random numbers
+ * @param start {number} - Smallest possible number
+ * @param end {number} - Biggest possible number
+ * @param n {number} - Number of random numbers
+ */
 export function range(start: number = 0, end: number = 1, n: number = 10) {
   const data = [];
   let item = 0;
