ff32950e4a6d7a54994d51e5d1c5023b7ec3d643
jhaboic <josiphaboic@gmail.com>
2018-09-15 01:07:55 +0200

    (update) range is now generator

--- __tests__/random/range.test.ts
+++ __tests__/random/range.test.ts
@@ -3,21 +3,21 @@ import * as range from '../../src/random/range';
 
 
 test('function range: returns 10 random numbers in range [0 ... 1]', () => {
-  expect(range.range()).toHaveLength(10);
+  expect([...range.range()]).toHaveLength(10);
 })
 
 test('function range: returns 5 random numbers in range [1 ... 10]', () => {
-  expect(range.range(1, 10, 5)).toHaveLength(5);
+  expect([...range.range(1, 10, 5)]).toHaveLength(5);
 })
 
 test('function range: returns 1 random number in range [1 ... 10]', () => {
-  expect(range.range(1, 10, 1)[0]).toBeGreaterThan(0);
+  expect([...range.range(1, 10, 1)][0]).toBeGreaterThan(0);
 })
 
 test('function range: returns 1 random numbers in range [10 ... 20]', () => {
-  expect(range.range(10, 20, 1)[0]).toBeGreaterThan(9);
+  expect([...range.range(10, 20, 1)][0]).toBeGreaterThan(9);
 })
 
 test('function range: returns 1 random numbers in range [-10 ... 0]', () => {
-  expect(range.range(-10, 0, 1)[0]).toBeGreaterThan(-11);
+  expect([...range.range(-10, 0, 1)][0]).toBeGreaterThan(-11);
 })
