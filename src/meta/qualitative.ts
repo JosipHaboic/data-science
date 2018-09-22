c4a610991b1d7beff346552f88ce12263649dd07
jhaboic <josiphaboic@gmail.com>
2018-09-23 00:49:34 +0200

    (update) edd more info and minimal examples

--- src/meta/qualitative.ts
+++ src/meta/qualitative.ts
@@ -1,2 +1,7 @@
 // Qualitative data is descriptive information (it describes something)
-export type QualitativeData = string | boolean;
+export type QualitativeData = {[key: string]: string | boolean | string[]};
+// example
+// const myDog: QualitativeData = {
+//   'legs': 'long',
+//   'color': ['brown', 'black'],
+// };
--- src/meta/quantitative.ts
+++ src/meta/quantitative.ts
@@ -2,4 +2,10 @@
 // It can be in these forms:
 //    Discrete data can only take certain values (like whole numbers)
 //    Continuous data can take any value (within a range)
-export type QuantitativeData = number | number[];
+export type QuantitativeData = {[key: string]: number | number[]};
+// example
+// const myDog: QuantitativeData = {
+//   legs: 4, // discrete
+//   weight: 25.5, // continous
+//   height: 565 // continous
+// };
