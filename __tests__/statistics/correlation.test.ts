efd37bc11527512108639583ff93c16d44728591
jhaboic <josiphaboic@gmail.com>
2018-09-29 21:49:13 +0200

    (update) add correlation function and fix covariance function

--- __tests__/statistics/correlation.test.ts
+++ __tests__/statistics/correlation.test.ts
@@ -9,7 +9,7 @@ test('function covariance: throws RangeError when passed non equal size data lis
   expect(() => correlation.covariance([1, 2], [1, 2, 3])).toThrowError(RangeError);
 })
 
-test('function covariance: returns 0 from list of [0, 0, 0]', () => {
+test('function covariance: returns 0 from lists [0, 0, 0] and [0, 0, 0]', () => {
   expect(correlation.covariance([0, 0, 0], [0, 0, 0])).toBe(0);
 })
 
@@ -17,3 +17,23 @@ test('function covariance: returns 2.85 from lists [2, 3, 2.7, 3.2, 4.1] and [10
   expect(correlation.covariance([2, 3, 2.7, 3.2, 4.1], [10, 14, 12, 15, 20])).toBeCloseTo(2.85);
 })
 
+
+test('function correlation: throws TypeError when not passed any data', () => {
+  expect(correlation.correlation).toThrowError(TypeError);
+})
+
+test('function correlation: throws RangeError when passed non equal size data lists', () => {
+  expect(() => correlation.correlation([1, 2], [1, 2, 3])).toThrowError(RangeError);
+})
+
+test('function correlation: returns 0 from lists [0, 0, 0] and [0, 0, 0]', () => {
+  expect(correlation.correlation([0, 0, 0], [0, 0, 0])).toBe(NaN);
+})
+
+test('function correlation: returns 1 from lists [1, 2, 3, 4, 5, 6] and [1, 2, 3, 4, 5, 6]', () => {
+  expect(correlation.correlation([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])).toBe(1);
+})
+
+test('function correlation: returns 1 from lists [6, 5, 4, 3, 2, 1] and [1, 2, 3, 4, 5, 6]', () => {
+  expect(correlation.correlation([6, 5, 4, 3, 2, 1], [1, 2, 3, 4, 5, 6])).toBe(-1);
+})
--- src/statistics/correlation.ts
+++ src/statistics/correlation.ts
@@ -1,25 +1,49 @@
-import { arithmeticMean } from '../statistics/summary';
+import { arithmeticMean, sum } from './summary';
 
 
 /**
  * Calculate covarinace from two data lists
- * @param {number[]} dataA - First data list
- * @param {number[]} dataB - Second data list
+ * @param {number[]} x - First data list
+ * @param {number[]} y - Second data list
  * @return {number}
  */
-export function covariance(dataA: number[], dataB: number[]): number {
-  if (dataA.length !== dataB.length) {
+export function covariance(x: number[], y: number[]): number {
+  if (x.length !== y.length) {
     throw new RangeError('Data lengths should be equal');
   }
-  const avgA = arithmeticMean(dataA);
-  const avgB = arithmeticMean(dataB);
+  const avgA = arithmeticMean(x);
+  const avgB = arithmeticMean(y);
 
   let result = 0;
 
-  for(let i = 0; i <= dataA.length - 1; i += 1) {
-    result += (dataA[i] - avgA) * (dataB[i] - avgB);
+  for(let i = 0; i <= x.length - 1; i += 1) {
+    result += (x[i] - avgA) * (y[i] - avgB);
   }
 
-  return result / (dataA.length - 1);
+  return result / (x.length - 1);
 }
 
+/**
+ * Calculate correlation between the two data lists
+ * @param {number} x - Number list
+ * @param {number} y - Number list
+ */
+export function correlation(x: number[], y: number[]) {
+  if (x.length !== y.length) {
+    throw new RangeError('Data lengths should be equal');
+  }
+
+  const meanX = arithmeticMean(x);
+  const meanY = arithmeticMean(y);
+
+  let xy = 0;
+  for (let i = 0; i <= x.length - 1; i += 1) {
+    xy += (x[i] - meanX) * (y[i] - meanY);
+  }
+
+
+  const a = sum(x.map((item: number) => Math.pow(item - meanX, 2)));
+  const b = sum(y.map((item: number) => Math.pow(item - meanY, 2)));
+
+  return xy / Math.sqrt(a * b);
+}
