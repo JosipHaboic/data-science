70f2945db010d2ba31ba0b573962986a0aa6e9bd
jhaboic <josiphaboic@gmail.com>
2018-09-30 12:33:09 +0200

    (fix) remove duplicate test

--- __tests__/algebra/sequence.test.ts
+++ __tests__/algebra/sequence.test.ts
@@ -5,11 +5,6 @@ test('sequnce:arithmetic - return [1, 2, 3] when (start=1, difference=1, n=3)', 
   expect([...sequence.arithmetic(1, 1, 3)]).toEqual([1, 2, 3]);
 });
 
-
-test('sequnce:linear - return [1, 2, 3] when (start=1, intercept=1, n=3)', () => {
-  expect([...sequence.linear(1, 1, 3)]).toEqual([1, 2, 3]);
-});
-
 test('sequnce:linear - return [2, 3, 4] when (start=1, intercept=2, n=3)', () => {
   expect([...sequence.linear(1, 2, 3)]).toEqual([2, 3, 4]);
 });
